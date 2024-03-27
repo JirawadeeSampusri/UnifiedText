b6e3a29eff640fff9d8bc70a0a6a974518637caf
JirawadeeSampusri <jirawadeesampu@gmail.com>
2024-03-27 13:48:05 +0100

    Implement Fetching Data

--- src/main.py
+++ src/main.py
@@ -8,16 +8,18 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <URL>")
         sys.exit(1)
-
+    
     url = sys.argv[1]
-
-    cmd = [NETCACHE, url]
-
+    
     try:
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        html_content = result.stdout
+        result = subprocess.run([NETCACHE, url], capture_output=True, text=True, check=True)
+        html_content = result.stdout.strip()
+        
+        print("Content from", url)
+        print(html_content)
 
         display_html_content(html_content)
 
     except subprocess.CalledProcessError as e:
         print("Error executing command:", e)
+  
